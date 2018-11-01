aeb9f0e57bbbb05fff1ddee59c67ff681966ac21
april <martinezapril56@yahoo.com>
2018-11-01 14:18:57 -0400

    filling in info

--- april/src/components/resume.js
+++ april/src/components/resume.js
@@ -19,18 +19,13 @@ class Resume extends Component
                 <h4 style={{ color: "grey" }}>Programmer</h4>
                 <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
                 <p>
-                  It is a long established fact that a reader will be
-                  distracted by the readable content of a page when
-                  looking at its layout. The point of using Lorem Ipsum
-                  is that it has a more-or-less normal distribution of
-                  letters, as opposed to using 'Content here, content
-                  here', making it look like readable English. Many
-                  desktop publishing packages and web page editors now
-                  use Lorem Ipsum as their default model text, and a
-                  search for 'lorem ipsum' will uncover many web sites
-                  still in their infancy. Various versions have evolved
-                  over the years, sometimes by accident, sometimes on
-                  purpose (injected humour and the like).
+                  Positive Attitude.
+                  Supreme Communication Skills.
+                  Great at Time and Task Management.
+                  Quick Learning Ability.
+                  Deep and Broad Technical Experience.
+                  A Good Team Player.
+                  These are a few things that make a good web developer.      
                 </p>
                 <hr style={{ borderTop: "3px solid #833fb2", width: "50%" }} />
                 <h5>Address</h5>
